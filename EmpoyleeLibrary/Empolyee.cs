7de2b21ec25a85452d96d370f8bf180bb266a294
FrankLikuohao <frank.likuohao@gmail.com>
2015-10-30 19:32:01 +0800

    作業
    已完成1.0版本

---
+++ EmpoyleeLibrary/Empolyee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpoyleeLibrary
+{
+    public class Empolyee
+    {
+        public int beneFit = 0;
+        public int baseSalary = 0;
+
+        public int Benefit
+        {
+            get
+            {
+                return beneFit;
+            }
+            set
+            {
+                if (value < 0) value = 0;
+                else
+                    beneFit = value;
+            }
+        }
+
+        public int BaseSalary
+        {
+            get
+            {
+                return baseSalary;
+            }
+            set
+            {
+                if (value < 0) value = 0;
+                else
+                    baseSalary = value;
+            }
+        }
+
+        public int Salary
+        {
+            get
+            {
+                return BaseSalary + Benefit;
+            }
+        }
+    }
+}
---
+++ PaySystem/Program.cs
@@ -0,0 +1,28 @@
+﻿using EmpoyleeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaySystem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Empolyee empolyee = new Empolyee();
+            int baseSalary = 0;
+            int beneFit = 0;
+            Console.WriteLine("請輸入該員工底薪：");
+            int.TryParse(Console.ReadLine(), out baseSalary);
+            empolyee.BaseSalary = baseSalary;
+            Console.WriteLine("請輸入該員工獎金：");
+            int.TryParse(Console.ReadLine(), out beneFit);
+            empolyee.Benefit = beneFit;
+            Console.WriteLine("員工總薪資為：" + empolyee.Salary);
+            Console.WriteLine("請按任意鍵離開");
+            Console.ReadLine();
+        }
+    }
+}
